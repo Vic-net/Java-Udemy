c9d2c74c5b8e834ce8fe105aa941169edaf0cf64
Renan Victor <renan.victor@edu.unifil.br>
2023-12-30 11:42:39 -0300

    Novas Classes

---
+++ Entities/Dollar.java
@@ -0,0 +1,10 @@
+package Entities;
+
+public class Dollar {
+    public static final double DOLLAR = 3.10;
+    public static final double IOF = 6.00;
+
+    public static double DollarInReais(double reais){
+        return reais * (DOLLAR*(1+(IOF/100)));
+    }
+}
